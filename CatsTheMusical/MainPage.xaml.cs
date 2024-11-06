6c66afe542637e2e286261e86fb581d7678714ad
TheFatKidfromZamosc <witiuk61@gmail.com>
2024-11-06 09:23:37 +0100

    This musical is chinese torture

---
+++ CatsTheMusical/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace CatsTheMusical
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+        
+            Cat1.Source= "https://cataas.com/cat";
+        }
+    }
+
+}
---
+++ CatsTheMusical/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CatsTheMusical
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
