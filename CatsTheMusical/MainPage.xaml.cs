2b09a33738bf6a65ff1a94753e6c4e46422c2f9c
TheFatKidfromZamosc <witiuk61@gmail.com>
2024-11-06 10:10:23 +0100

    Pee is stored in the balls

--- CatsTheMusical/MainPage.xaml.cs
+++ CatsTheMusical/MainPage.xaml.cs
@@ -11,11 +11,27 @@ namespace CatsTheMusical
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnCounterClicked1(object sender, EventArgs e)
         {
-        
-            Cat1.Source= "https://cataas.com/cat";
+            Cat1.Source= "https://cataas.com/cat/says/"+ CatText.Text;
         }
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            Cat2.Source = "https://cataas.com/cat/says/" + CatText.Text + "?fontColor=" + CatTextColor.Text;
+        }
+        private void OnCounterClicked3(object sender, EventArgs e)
+        {
+            Cat3.Source = "https://cataas.com/cat"+ "?filter=custom&brightness=" + CatBrightnes.Text;
+        }
+        private void OnCounterClicked4(object sender, EventArgs e)
+        {
+            Cat4.Source = "https://cataas.com/cat" + "?filter=custom&brightness=" + CatBrightnes.Text+"&saturation=" +CatSaturation.Text;
+        }
+        private void OnCounterClicked5(object sender, EventArgs e)
+        {
+            Cat5.Source = "https://cataas.com/cat" + "?filter="+CatFilter.Text+"&brightness=" + CatBrightnes.Text + "&saturation=" + CatSaturation.Text+ "&filter ="+CatFilter.Text;
+        }
+
     }
 
 }
