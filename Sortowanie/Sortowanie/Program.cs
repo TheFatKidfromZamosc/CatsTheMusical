c7ad36e01c2e0b167092f97b51f77ec1ea31b786
TheFatKidfromZamosc <witiuk61@gmail.com>
2025-01-09 09:14:38 +0100

    Always remember

---
+++ Sortowanie/Sortowanie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 800, 11, 50, 771, 649, 770, 240, 9 };
+
+        int temp = 0;
+        // sortowanie bombelkowe
+        for (int write = 0; write < arr.Length; write++)
+        {
+            for (int sort = 0; sort < arr.Length - 1; sort++)
+            {
+                if (arr[sort] > arr[sort + 1])
+                {
+                    temp = arr[sort + 1];
+                    arr[sort + 1] = arr[sort];
+                    arr[sort] = temp;
+                }
+            }
+        }
+        // wyswietlanie posortowanej tablicy
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+            i+=2;
+        }
+    }
+}
---
+++ TwinTowers/TwinTowers/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program counts the amount of speed an object can get if they were to fall for the amount of time specified by the user
+// Input : 10 s
+//Output : 490 m
+
+namespace TwinTowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The time the object will free fall in seconds : ");
+            int t = Convert.ToInt32(Console.ReadLine());
+            double distance = (0.5*9.8*(t));
+            Console.WriteLine("The distance the object has fallen :" + distance );
+        }
+    }
+}
